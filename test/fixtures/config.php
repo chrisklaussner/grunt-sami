c72d18031921931056f23054c35a8bfb8529ff65
Christian Klaussner <developer@klaussner.me>
2016-05-14 16:18:36 +0200

    Format test configuration

--- test/fixtures/config.php
+++ test/fixtures/config.php
@@ -9,13 +9,13 @@ $iterator = Finder::create()
   ->name('*.php')
   ->in('test/fixtures');
 
-// Store the documentation in the tmp folder next to Gruntfile.js.
+// Store the documentation in the tmp folder next to Gruntfile.js
 $sami = new Sami($iterator, array(
   'build_dir' => __DIR__.'/../../tmp/build',
   'cache_dir' => __DIR__.'/../../tmp/cache'
 ));
 
-// Include private members.
+// Include private members
 $sami['filter'] = function() {
   return new TrueFilter();
 };
